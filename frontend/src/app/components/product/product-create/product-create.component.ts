6ad9c93f06f46bc290192e97a238e6ed289c9f9d
Ramon Moises <ramon.firmo@infraestrutura.gov.br>
2021-10-27 18:04:32 -0300

    Arrumando Bugs

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -24,7 +24,7 @@ export class ProductCreateComponent implements OnInit {
 
   createProduct(): void {
     this.productService.create(this.product).subscribe(() => {
-      this.productService.showMessage('Produto criado!')
+      this.productService.showMessage('Paciente criado!')
       this.router.navigate(['/products'])
     })
 
